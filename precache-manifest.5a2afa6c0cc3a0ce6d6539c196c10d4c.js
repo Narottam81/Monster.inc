80ac92d8ed63b31342199278668d58dd6bfcab10
Narottam81 <narottambasnet81@gmail.com>
2020-10-24 00:57:36 +0545

    Updates

---
+++ precache-manifest.5a2afa6c0cc3a0ce6d6539c196c10d4c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "842048fe93d1f33be86fa4a448a1e0b9",
+    "url": "/Monster.inc/index.html"
+  },
+  {
+    "revision": "d6e2703616725ab8f30c",
+    "url": "/Monster.inc/static/css/main.1f3a4bd1.chunk.css"
+  },
+  {
+    "revision": "0417d0a48f626b24917d",
+    "url": "/Monster.inc/static/js/2.962deeea.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/Monster.inc/static/js/2.962deeea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6e2703616725ab8f30c",
+    "url": "/Monster.inc/static/js/main.03946186.chunk.js"
+  },
+  {
+    "revision": "717511a3f633be6ba394",
+    "url": "/Monster.inc/static/js/runtime-main.36ede7bc.js"
+  }
+]);
